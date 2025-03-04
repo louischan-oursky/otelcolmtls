75d16140ca52cedfed87af4b75e4cc72904b02b5
Louis Chan <louischan@oursky.com>
2025-03-04 19:24:24 +0800

    Install telemetrygen

--- flake.nix
+++ flake.nix
@@ -1,9 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-    };
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
@@ -17,26 +15,31 @@
       let
         pkgs = import nixpkgs {
           inherit system;
-          # As of 2025-02-07, 1.23.6 is still unavailable in nixpkgs-unstable, so we need to use overlay to build 1.23.6 ourselves.
-          overlays = [
-            (final: prev: {
-              go = (
-                prev.go.overrideAttrs {
-                  version = "1.23.6";
-                  src = prev.fetchurl {
-                    url = "https://go.dev/dl/go1.23.6.src.tar.gz";
-                    hash = "sha256-A5xbBOZSedrO7opvcecL0Fz1uAF4K293xuGeLtBREiI=";
-                  };
-                }
-              );
-            })
-          ];
         };
       in
       {
         devShells.default = pkgs.mkShell {
           packages = [
+            # Any Go version is fine.
             pkgs.go
+
+            (pkgs.buildGoModule {
+              name = "telemetrygen";
+              src = pkgs.fetchFromGitHub {
+                owner = "open-telemetry";
+                repo = "opentelemetry-collector-contrib";
+                rev = "v0.120.1";
+                sha256 = "sha256-U73wpwQrlbfnhlVIN5GcoTzl1fOcBh8zkRgbPLZvHUI=";
+              };
+              vendorHash = "sha256-yCCfW21gaKdXMh/0Gop/v8h/p6i2JFyN305UtOJRwP8=";
+              modRoot = "./cmd/telemetrygen";
+              # internal/e2etest is another go module.
+              # That will confuse buildGoModule as it does not know how to build nested go module.
+              # We remove it.
+              prePatch = ''
+                rm -r ./cmd/telemetrygen/internal/e2etest
+              '';
+            })
           ];
         };
       }
